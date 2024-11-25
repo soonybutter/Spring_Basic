a12f4b3ed9530c9d270caa4b25788b6768ff4c14
Yang Dayeon <ydy229@naver.com>
2024-11-26 00:08:43 +0900

    [Spring_TestAOP] commit.

---
+++ src/com/test/entity/Rectangle.java
@@ -0,0 +1,22 @@
+package com.test.entity;
+
+public class Rectangle extends Shape {
+	
+	public Rectangle() {}
+	
+	public Rectangle(String title, int width, int height) {
+		super(title, width, height);
+	}
+
+	
+	@Override
+	public void viewSize() {
+		System.out.println(getTitle() + "의 넓이: "+ (getWidth() * getHeight() ));
+	}
+
+	
+
+	
+	
+
+}
