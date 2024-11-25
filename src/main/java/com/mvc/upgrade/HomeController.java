75f581b1e9c0d0ee3bdb381362437cbaf5ee7fd1
Yang Dayeon <ydy229@naver.com>
2024-11-26 00:28:10 +0900

    [SpringMVC_filter_aop_login_interceptor_transaction] commit

---
+++ src/main/java/com/mvc/upgrade/HomeController.java
@@ -0,0 +1,130 @@
+package com.mvc.upgrade;
+
+
+import java.util.List;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mvc.upgrade.model.biz.BoardBiz;
+import com.mvc.upgrade.model.dto.BoardDto;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private BoardBiz biz;
+	
+	@RequestMapping("/list.do")
+	public String list(Model model) {
+		
+		logger.info("SELECT LIST");
+		
+		List<BoardDto> res = biz.selectList();
+		model.addAttribute("list",res);
+		
+		return "mvclist";
+	
+	}
+	
+	@RequestMapping("/detail.do")
+	public String detail(Model model, int myno) {
+		logger.info("SELECT ONE");
+		model.addAttribute("dto",biz.selectOne(myno));
+		
+		return "mvcdetail";
+	}
+	
+	@RequestMapping("/insertform.do")
+	public String insertForm() {
+		logger.info("INSERT FORM");
+		
+		
+		return "mvcinsert";
+	}
+	
+	@RequestMapping("/insert.do")
+	public String insert(BoardDto dto) {
+		logger.info("INSERT");
+		
+		int res = biz.insert(dto);
+		
+		if(res>0) {
+			biz.selectList();
+			return "redirect:list.do";
+		}
+		else {
+			return "redirect:insertform.do";
+		}
+		
+	}
+	
+	@RequestMapping("/updateform.do")
+	public String updateForm(Model model, int myno) {
+		
+		logger.info("UPDATE FORM");
+		BoardDto res = biz.selectOne(myno);
+		model.addAttribute("dto", res);
+		
+		return "mvcupdate";
+		
+	}
+	
+	@RequestMapping("/update.do")
+	public String update(int myno, String mytitle, String mycontent) {
+		logger.info("UPDATE");
+	
+		BoardDto dto = new BoardDto();
+		dto.setMyno(myno);
+		dto.setMytitle(mytitle);
+		dto.setMycontent(mycontent);
+		
+		int res = biz.update(dto);
+		if(res>0) {
+			return "redirect:detail.do?myno="+dto.getMyno();
+		}
+		else {
+			return "redirect:updateform.do?myno="+dto.getMyno();
+		}
+		
+		
+	}
+	
+	//삭제
+	@RequestMapping("/delete.do")
+	public String delete(int myno) {
+		logger.info("DELETE");
+		
+		int res = biz.delete(myno);
+		if(res>0) {
+			return "redirect:list.do";
+		}
+		else {
+			return "redirect:detail.do?myno="+myno;
+		}
+		
+	}
+	
+	@RequestMapping("/test.do")
+		public String test() {
+			logger.info("TRANSACTION");
+			biz.test();
+			
+			return "redirect:list.do";
+			
+		}
+		
+	
+	
+	
+	
+	
+}
+
---
+++ src/main/java/com/mvc/upgrade/model/biz/BoardBizImpl.java
@@ -0,0 +1,62 @@
+package com.mvc.upgrade.model.biz;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mvc.upgrade.model.dao.BoardDao;
+import com.mvc.upgrade.model.dto.BoardDto;
+
+@Service
+public class BoardBizImpl implements BoardBiz {
+
+	@Autowired
+	private BoardDao dao;
+	
+	@Override
+	public List<BoardDto> selectList() {
+		List<BoardDto> res= dao.selectList();
+		
+		return res;
+	}
+
+	@Override
+	public BoardDto selectOne(int myno) {
+		return dao.selectOne(myno);
+	}
+
+	@Override
+	public int insert(BoardDto dto) {
+		
+		return dao.insert(dto);
+	}
+
+	@Override
+	public int update(BoardDto dto) {
+		
+		return dao.update(dto);
+	}
+
+	@Override
+	public int delete(int myno) {
+		
+		return dao.delete(myno);
+	}
+
+	@Override
+	@Transactional 
+	public String test() {
+		dao.insert(new BoardDto(0,"transaction1","test","insert",null));
+		
+		String test = dao.test();
+		
+		//null.length() -> insert 후 exception 발생시킴.
+		//test.length();
+		
+		return null;
+	}
+	
+
+}
