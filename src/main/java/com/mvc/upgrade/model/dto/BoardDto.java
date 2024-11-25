9fc1cc77d5baeb549b877589672a0f288563c19c
Yang Dayeon <ydy229@naver.com>
2024-11-26 00:17:07 +0900

    [Spring_MVC02] commit.

---
+++ src/main/java/com/mvc/upgrade/model/dto/BoardDto.java
@@ -0,0 +1,25 @@
+package com.mvc.upgrade.model.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+// =@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class BoardDto {
+	private int myno;
+	private String myname;
+	private String mytitle;
+	private String mycontent;
+	private Date mydate;
+	
+
+}
